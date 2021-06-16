4a0308d653dd1e561048872fbcc6b801bba0512c
Tini Vasanth <tini.vasanth@gmail.com>
2021-06-16 10:52:38 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var input,heading;
+function setup()
+{
+  createCanvas(400,400);
+
+  input=createInput();
+  input.position(5,60);
+
+  heading=createElement("h4","Enter any grade");
+  heading.position(5,20)
+
+}
+function draw()
+{
+  background("green")
+  
+}
+
+
+   
